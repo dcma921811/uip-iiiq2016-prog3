edba47efe5748ad74415bc00545e49e9fd5cb39a
Unknown <dcma.92@gmail.com>
2016-09-14 21:13:53 -0500

    Actualización
    Se añaden las carpetas correspondientes

---
+++ laboratorios/Laboratorio 2/main.py
@@ -0,0 +1,12 @@
+
+euro = 1 * 0.8965
+yen = 1 * 101.5744
+libra = 1 * 0.7702
+pesos = 1 * 19.7843
+
+print("El valor de un dolar en diferentes monedas: ")
+
+print("Euro " + str(euro))
+print("Yen " + str(yen))
+print("Libra " + str(libra))
+print("Pesos " + str(pesos))
